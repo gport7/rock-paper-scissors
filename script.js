5ce3d7474837802c3d18ad648ca665ca93a14b5d
gport7 <gport@vt.edu>
2022-03-04 18:14:25 -0800

    clean the code so that it is not case sensitive on player input

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let playerScore = 0;
 //computer chooses Rock, Paper, or Scissors randomly and returns the result
 function computerPlay () {
     let randomNumber = Math.random ();
+    let choice;
     if (randomNumber < 0.33) {
         choice = "Rock";
     } else if (randomNumber < 0.66) {
@@ -17,12 +18,20 @@ function computerPlay () {
     return choice;
 }
 
+//player chooses Rock, Paper, or Scissors in a non-case-sensitive way
+function playerPlay () {
+    let input = prompt("Rock, Paper, or Scissors?");
+    let choice = input[0].toUpperCase() + input.substring(1).toLowerCase();
+    console.log("Player choice: " + choice)
+    return choice;
+}
+
+
 //play one round
 function playRound (computerSelection) {
-    playerSelection = prompt("Rock, Paper, or Scissors?");
-    console.log("Player choice: " + playerSelection)
+    playerSelection = playerPlay();
     computerSelection = computerPlay();
-    if (computerSelection === playerSelection) {
+    if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
         console.log("This round is a tie!");      
     } else if (computerSelection === "Rock" && playerSelection === "Scissors" ||
                 computerSelection === "Scissors" && playerSelection === "Paper" ||
