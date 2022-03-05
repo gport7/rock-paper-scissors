9140a70e97b60c1f2cb5cb59256c4ba36cc527ff
gport7 <gport@vt.edu>
2022-03-04 17:27:58 -0800

    added score keeping

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 let computerSelection;
+let computerScore = 0;
+let playerSelection;
+let playerScore = 0;
 
+//computer chooses Rock, Paper, or Scissors randomly and returns the result
 function computerPlay () {
-    randomNumber = Math.random ();
+    let randomNumber = Math.random ();
     if (randomNumber < 0.33) {
-        computerSelection = "Rock";
+        choice = "Rock";
     } else if (randomNumber < 0.66) {
-        computerSelection = "Paper";
+        choice = "Paper";
     } else {
-        computerSelection = "Scissors";
+        choice = "Scissors";
     }
-    console.log(computerSelection);
-    return computerSelection;
+    console.log("Computer choice: " + choice);
+    return choice;
 }
 
-computerPlay();
+//play one round
+function playRound (computerSelection) {
+    playerSelection = prompt("Rock, Paper, or Scissors?");
+    console.log("Player choice: " + playerSelection)
+    computerSelection = computerPlay();
+    if (computerSelection === playerSelection) {
+        console.log("It's a tie!");
+        showScore();
+    } else if (computerSelection === "Rock" && playerSelection === "Scissors" ||
+                computerSelection === "Scissors" && playerSelection === "Paper" ||
+                computerSelection === "Paper" && playerSelection === "Rock") {
+        console.log("Computer wins!");
+        computerScore++;
+        showScore();
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+    playerSelection === "Scissors" && computerSelection === "Paper" ||
+    playerSelection === "Paper" && computerSelection === "Rock") {
+        console.log("Player wins!");
+        playerScore++;
+        showScore();
+    } else {
+        console.log("Player is disqualified for choosing something other than the allowed items!");
+    }
+}
+
+//show score
+function showScore () {
+    console.log("Player: " + playerScore + ", Computer: " + computerScore);
+}
+
+playRound();
